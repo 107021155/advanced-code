7b189c94c592d1f2325e49382b0ee708014e8522
107021155 <43198527+107021155@users.noreply.github.com>
2019-04-10 14:21:24 +0800

    Add files via upload

---
+++ 第四題/src/MainFrame.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame {
+  //  private TextField tf = new TextField();
+    private JLabel jbl = new JLabel(" ");
+    private JButton jb1 = new JButton("ADD");
+    private JButton jb2 = new JButton("SUB");
+    private Container cp;
+    private  int count = 0;
+
+
+
+    public  MainFrame(){
+        init();
+    }
+    private void init(){
+        cp=this.getContentPane();
+        cp.setLayout(null);
+        this.setSize(600,500);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jbl.setBounds(180,40,80,25);
+        jb1.setBounds(100,70,80,25);
+        jb2.setBounds(200,70,80,25);
+
+        jb1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count++;
+                jbl.setText(Integer.toString(count));
+            }
+        });
+        jb2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count--;
+                jbl.setText(Integer.toString(count));
+            }
+        });
+
+        cp.add(jbl);
+        cp.add(jb1);
+        cp.add(jb2);
+    }
+}
